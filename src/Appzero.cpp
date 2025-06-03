eef92f52a66259a77b9ca939a2ffcfa488267759
cancam <t112590050@ntut.org.tw>
2025-06-03 13:04:44 +0800

    final

--- src/Appzero.cpp
+++ src/Appzero.cpp
@@ -22,7 +22,7 @@ void App::zero() {
     gameovertime=0;
     lives=3;
     if(Util::Input::IsKeyDown(Util::Keycode::R)) {
-        m_CurrentState=State::TWO;
+        m_CurrentState=State::START;
         firstzero=0;
     }
     if (Util::Input::IsKeyPressed(Util::Keycode::ESCAPE) || Util::Input::IfExit()) {
